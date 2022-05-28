4e80b349fdb00e0047768ce2edb218b8f9f80347
JoyAnneW <joyawilliams@gmail.com>
2022-05-28 10:24:07 +0200

    installed bcrypt and jsonwebtoken. updated init_db file to reflect new db structure

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ function App() {
 	}, []);
 
 	return (
-		<div className="container">
+		<div className="">
 			<Header />
 			<button
 				className={`btn ${isAdmin ? "btn-outline-danger" : ""}`}
--- model/init_db.sql
+++ model/init_db.sql
@@ -1,43 +1,55 @@
-DROP TABLE if exists `lawyers`; 
-CREATE TABLE `lawyers` (
+CREATE TABLE `users` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`first_name` varchar(40),
+	`first_name` varchar(40) NOT NULL,
 	`last_name` varchar(40) NOT NULL,
 	`email` varchar(40) NOT NULL,
 	`phone` varchar(15) NOT NULL,
-	`specialty` varchar(20) NOT NULL,
-	`available` BOOLEAN NOT NULL DEFAULT '1',
+	`role` varchar(11) NOT NULL,
+	`password` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-DROP TABLE if exists `requests`; 
 CREATE TABLE `requests` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`description` TEXT NOT NULL,
 	`accepted` BOOLEAN NOT NULL DEFAULT '0',
+	`assigned` BOOLEAN NOT NULL DEFAULT '0',
 	`completed` BOOLEAN NOT NULL DEFAULT '0',
-	`requester_id` INT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-DROP TABLE if exists `requesters`; 
 CREATE TABLE `requesters` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`first_name` varchar(40),
-	`last_name` varchar(40) NOT NULL,
-	`email` varchar(40) NOT NULL,
-	`phone` varchar(15) NOT NULL,
 	`needs` varchar(20) NOT NULL,
+	`user_id` INT NOT NULL,
+	`request_id` INT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `lawyers_requests` (
-	`lawyer_id` INT NOT NULL,
+CREATE TABLE `lawyer_assignments` (
+	`user_id` INT NOT NULL,
 	`request_id` INT NOT NULL
 );
 
-ALTER TABLE `requests` ADD CONSTRAINT `requests_fk0` FOREIGN KEY (`requester_id`) REFERENCES `requesters`(`id`);
+CREATE TABLE `lawyers` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`specialty` varchar(20) NOT NULL,
+	`available` BOOLEAN NOT NULL DEFAULT '1',
+	`user_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-ALTER TABLE `lawyers_requests` ADD CONSTRAINT `lawyers_requests_fk0` FOREIGN KEY (`lawyer_id`) REFERENCES `lawyers`(`id`);
+ALTER TABLE `requesters` ADD CONSTRAINT `requesters_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
 
-ALTER TABLE `lawyers_requests` ADD CONSTRAINT `lawyers_requests_fk1` FOREIGN KEY (`request_id`) REFERENCES `requests`(`id`);
+ALTER TABLE `requesters` ADD CONSTRAINT `requesters_fk1` FOREIGN KEY (`request_id`) REFERENCES `requests`(`id`);
+
+ALTER TABLE `lawyer_assignments` ADD CONSTRAINT `lawyer_assignments_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+ALTER TABLE `lawyer_assignments` ADD CONSTRAINT `lawyer_assignments_fk1` FOREIGN KEY (`request_id`) REFERENCES `requests`(`id`);
+
+ALTER TABLE `lawyers` ADD CONSTRAINT `lawyers_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+
+
+
+
