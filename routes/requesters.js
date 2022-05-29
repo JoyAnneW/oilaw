77151873d8c85e15b49818ab5f8c52bee3dc4ef4
JoyAnneW <joyawilliams@gmail.com>
2022-05-29 22:03:27 +0200

    changed the return object following the post that creates a new requester and request. the new object returns all data from the three tables related to the requester and her request

--- routes/requesters.js
+++ routes/requesters.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../model/helper");
 
-// create new requester
+// create new requester and request
 // /api/requesters
 router.post("/", async (req, res) => {
 	const { first_name, last_name, email, phone, description, contact_pref } =
@@ -10,11 +10,13 @@ router.post("/", async (req, res) => {
 
 	// requesters table has fks from users and requests. insert into those first, then get the id from those tables using max(id)
 	// description, first and last name are wrapped in double quotes because apostrophes in submission throw an error.
-	const sqlInsert = `INSERT INTO users (first_name, last_name, email, phone, role, password) VALUES ("${first_name}", "${last_name}", '${email}', '${phone}', 'requester', 'N/A'); INSERT INTO requests (description, created_at) VALUES ("${description}", now()); INSERT INTO requesters (user_id, request_id, contact_pref) VALUES ((SELECT max(id) FROM users), (SELECT max(id) from requests), '${contact_pref}');`;
+	const sqlInsert = `INSERT INTO users (first_name, last_name, email, phone, role, password) VALUES ("${first_name}", "${last_name}", '${email}', '${phone}', 'requester', 'N/A'); INSERT INTO requesters (user_id, contact_pref) VALUES ((SELECT max(id) FROM users), '${contact_pref}'); INSERT INTO requests (requester_id, description, created_at) VALUES ((SELECT max(id) FROM requesters), "${description}", now());`;
 
 	try {
 		let results = await db(sqlInsert);
-		results = await db("SELECT * FROM requesters");
+		// return a combined object from the 3 tables with all info about a request
+		const sqlJoin = `SELECT  users.id, users.first_name, users.last_name, users.email, users.phone, requesters.id, requesters.contact_pref, requests.* FROM requesters INNER JOIN users ON requesters.user_id=users.id INNER JOIN requests ON requesters.id=requests.requester_id;`;
+		results = await db(sqlJoin);
 		if (results.data.length) {
 			res.status(200).send(results.data);
 		} else {
